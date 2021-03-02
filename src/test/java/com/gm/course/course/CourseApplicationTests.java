5c65aca8f806c7477e300c01264a9ba81126fba8
GM <4941333@qq.com>
2021-03-02 10:49:34 +0800

    spring  初始项目

---
+++ src/test/java/com/gm/course/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.course.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
