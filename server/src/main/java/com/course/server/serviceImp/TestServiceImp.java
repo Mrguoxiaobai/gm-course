456d5e2d3afe48bd927473504ce23f06bddf4866
GM <4941333@qq.com>
2021-03-05 13:53:38 +0800

    1.新建公共模块server 2.system模块集成server公共模块

--- server/src/main/java/com/course/server/serviceImp/TestServiceImp.java
+++ server/src/main/java/com/course/server/serviceImp/TestServiceImp.java
@@ -1,12 +1,13 @@
-package com.course.system.serviceImp;
+package com.course.server.serviceImp;
 
-import com.course.system.domain.Test;
-import com.course.system.mapper.TestMapper;
-import com.course.system.service.TestService;
+import com.course.server.domain.Test;
+import com.course.server.mapper.TestMapper;
+import com.course.server.service.TestService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+
 @Service
 public class TestServiceImp implements TestService {
     @Resource
