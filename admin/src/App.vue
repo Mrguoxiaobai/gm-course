78a3d80191b10770b7f5cabed74a8d521cb8ef4a
GM <4941333@qq.com>
2021-03-10 16:27:57 +0800

    1.修改admin 中的登录页面背景 2.增加admin-element vue项目 实现登录界面

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -82,6 +82,25 @@ export default {
   name: 'App',
 
 }
+
+window.onload = function () {
+  //配置
+  var config = {
+    vx: 4,	//小球x轴速度,正为右，负为左
+    vy: 4,	//小球y轴速度
+    height: 2,	//小球高宽，其实为正方形，所以不宜太大
+    width: 2,
+    count: 600,		//点个数
+    color: "64,158,255", 	//点颜色
+    stroke: "64,158,255", 	//线条颜色
+    dist: 6000, 	//点吸附距离
+    e_dist: 20000, 	//鼠标吸附加速距离
+    max_conn: 10 	//点到点最大连接数
+  };
+  //调用
+  CanvasParticle(config);
+}
+
 </script>
 
 
