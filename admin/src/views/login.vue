1129a6e77bccffff31b3b8a684063b025e160926
GM <4941333@qq.com>
2021-03-11 09:32:29 +0800

    1.解决admin  login页面STJKS 管理系统登录 标题不显示 问题

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -7,10 +7,9 @@
             <div class="center">
               <h1>
                 <i class="ace-icon fa fa-leaf green"></i>
-                <span class="red">Ace</span>
-                <span class="white" id="id-text2">Application</span>
+                <span class="red">STJKS</span>
+                <span class="white" id="id-text2"> 管理系统登录</span>
               </h1>
-              <h4 class="blue" id="id-company-text">&copy; Company Name</h4>
             </div>
 
             <div class="space-6"></div>
@@ -23,67 +22,44 @@
                       <i class="ace-icon fa fa-coffee green"></i>
                       请输入用户名密码
                     </h4>
-
                     <div class="space-6"></div>
-
                     <form>
                       <fieldset>
                         <label class="block clearfix">
-                          <span class="block input-icon input-icon-right">
-                            <input  type="text" class="form-control" placeholder="用户名"/>
-                            <i class="ace-icon fa fa-user"></i>
-                          </span>
+														<span class="block input-icon input-icon-right">
+															<input type="text" class="form-control" placeholder="Username" />
+															<i class="ace-icon fa fa-user"></i>
+														</span>
                         </label>
-
                         <label class="block clearfix">
-                          <span class="block input-icon input-icon-right">
-                            <input  type="password" class="form-control" placeholder="密码"/>
-                            <i class="ace-icon fa fa-lock"></i>
-                          </span>
+														<span class="block input-icon input-icon-right">
+															<input type="password" class="form-control" placeholder="Password" />
+															<i class="ace-icon fa fa-lock"></i>
+														</span>
                         </label>
-
-                        <label class="block clearfix">
-                          <span class="block input-icon input-icon-right">
-                            <div class="input-group">
-                              <input  type="text" class="form-control" placeholder="验证码">
-                              <span class="input-group-addon" id="basic-addon2">
-                                <img  id="image-code" alt="验证码"/>
-                              </span>
-                            </div>
-                          </span>
-                        </label>
-
                         <div class="space"></div>
-
                         <div class="clearfix">
                           <label class="inline">
-                            <input  type="checkbox" class="ace"/>
-                            <span class="lbl">记住我</span>
+                            <input type="checkbox" class="ace" />
+                            <span class="lbl"> 记住我</span>
                           </label>
-
-                          <button type="button"
-                                  class="width-35 pull-right btn btn-sm btn-primary"
-                                  v-on:click="login()">
+                          <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
                             <i class="ace-icon fa fa-key"></i>
                             <span class="bigger-110">登录</span>
                           </button>
                         </div>
-
                         <div class="space-4"></div>
                       </fieldset>
                     </form>
-
                   </div><!-- /.widget-main -->
-
                 </div><!-- /.widget-body -->
               </div><!-- /.login-box -->
             </div><!-- /.position-relative -->
-
           </div>
         </div><!-- /.col -->
       </div><!-- /.row -->
     </div><!-- /.main-content -->
-  </div>
+  </div><!-- /.main-container -->
 </template>
 
 <script>
